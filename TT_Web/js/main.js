51c5d321c047da0064957c2774bf7872f64a750b
Tas <tas0173@gmail.com>
2020-09-19 10:58:13 +1000

    Update main.js

--- TT_Web/js/main.js
+++ TT_Web/js/main.js
@@ -18,18 +18,6 @@
         $(".loader").fadeOut();
         $("#preloder").delay(200).fadeOut("slow");
 
-        /*------------------
-            Car filter
-        --------------------*/
-        $('.filter__controls li').on('click', function () {
-            $('.filter__controls li').removeClass('active');
-            $(this).addClass('active');
-        });
-        if ($('.car-filter').length > 0) {
-            var containerEl = document.querySelector('.car-filter');
-            var mixer = mixitup(containerEl);
-        }
-    });
 
     /*------------------
         Background Set
@@ -82,60 +70,10 @@
         autoplay: false
     });
 
-    /*--------------------------
-        Testimonial Slider
-    ----------------------------*/
-    var testimonialSlider = $(".testimonial__slider");
-    testimonialSlider.owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 2,
-        dots: true,
-        nav: true,
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: false,
-        responsive: {
-            768: {
-                items: 2
-            },
-            0: {
-                items: 1
-            }
-        }
-    });
-
-    /*-----------------------------
-        Car thumb Slider
-    -------------------------------*/
-    $(".car__thumb__slider").owlCarousel({
-        loop: true,
-        margin: 25,
-        items: 5,
-        dots: false,
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-        mouseDrag: false,
-        responsive: {
-
-            768: {
-                items: 5
-            },
-            320: {
-                items: 3
-            },
-            0: {
-                items: 2
-            }
-        }
-    });
-
     /*-----------------------
 		Range Slider
 	------------------------ */
-    var rangeSlider = $(".price-range");
+    var rangeSlider = $(".salary-range");
     rangeSlider.slider({
         range: true,
         min: 1,
@@ -145,19 +83,19 @@
             $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1] + "");
         }
     });
-    $("#amount").val("$" + $(".price-range").slider("values", 0) + " - $" + $(".price-range").slider("values", 1) + "");
+    $("#amount").val("$" + $(".salary-range").slider("values", 0) + " - $" + $(".salary-range").slider("values", 1) + "");
 
-    var carSlider = $(".car-price-range");
-    carSlider.slider({
+    var jobSlider = $(".job-salary-range");
+    jobSlider.slider({
         range: true,
         min: 1,
         max: 4000,
         values: [900, 3000],
         slide: function (event, ui) {
-            $("#caramount").val("$" + ui.values[0] + " - $" + ui.values[1] + "");
+            $("#salaryamount").val("$" + ui.values[0] + " - $" + ui.values[1] + "");
         }
     });
-    $("#caramount").val("$" + $(".car-price-range").slider("values", 0) + " - $" + $(".car-price-range").slider("values", 1) + ".100");
+    $("#jobamount").val("$" + $(".job-salary-range").slider("values", 0) + " - $" + $(".salary-range-range").slider("values", 1) + ".100");
 
     var filterSlider = $(".filter-price-range");
     filterSlider.slider({
@@ -177,21 +115,14 @@
     $("select").niceSelect();
 
     /*------------------
-		Magnific
-	--------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
-
-    /*------------------
 		Single Product
 	--------------------*/
-    $('.car-thumbs-track .ct').on('click', function () {
-        $('.car-thumbs-track .ct').removeClass('active');
+    $('.job-thumbs-track .ct').on('click', function () {
+        $('.job-thumbs-track .ct').removeClass('active');
         var imgurl = $(this).data('imgbigurl');
-        var bigImg = $('.car-big-img').attr('src');
+        var bigImg = $('.job-big-img').attr('src');
         if (imgurl != bigImg) {
-            $('.car-big-img').attr({
+            $('.job-big-img').attr({
                 src: imgurl
             });
         }
