b67bd0ce4e4fda489b7bb2949606b23b304bb6d7
babetteblanquet <65524644+babetteblanquet@users.noreply.github.com>
2020-09-19 13:50:18 +1000

    Days of the week

--- Babette/DayOfTheWeek/Example_daysoftheweek.js
+++ Babette/DayOfTheWeek/Example_daysoftheweek.js
@@ -1,3 +1,13 @@
+
+var created = ["2020-08-24T14:08:55Z",
+    "2020-09-08T03:28:55Z",
+];
+
+console.log(created);
+
+var newDate = new Date("2020-09-08T03:28:55Z").toString();
+console.log(newDate);
+
 const dates = [
     '2018-04-19',
     '2018-04-19',
@@ -7,26 +17,58 @@ const dates = [
     '2018-05-02',
     '2018-05-02',
     '2018-05-02',
-  ];
-  console.log(dates);
+    "2020-08-24T14:08:55Z",
+    "2020-09-08T03:28:55Z",
+];
+console.log(dates);
 
-  const daysOfWeek = dates.map(date => new Date(date).getDay());
-  console.log(daysOfWeek)
-  
-  // Now we have the days as numbers we can sort by frequency
-  // This function is from a google search: https://stackoverflow.com/a/3579651/414062
-  function sortByFrequency(array) {
+const daysOfWeek = dates.map(date => new Date(date).getDay());
+console.log(daysOfWeek)
+
+// Now we have the days as numbers we can sort by frequency
+// This function is from a google search: https://stackoverflow.com/a/3579651/414062
+function Frequency(array) {
     const frequency = {};
-  
+
     array.forEach(value => frequency[value] = 0);
     const uniques = array.filter(value => ++frequency[value] == 1);
-    return uniques.sort((a, b) => frequency[b] - frequency[a]);
-  }
-  
-  const sortedDaysOfWeek = sortByFrequency(daysOfWeek);
-  
-  // Now translate to weekday values
-  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  const weekdaysByFrequency = sortedDaysOfWeek.map(day => weekdays[day - 1]);
-  
-  console.log(weekdaysByFrequency);
+    return frequency;
+    // return uniques.sort((a, b) => frequency[b] - frequency[a]);
+}
+
+//Passing in our dates in the function Frequency so we get a dictionary of days and frequencies of job ads:
+const DaysOfWeek = Frequency(daysOfWeek);
+
+//TO DELETE LATER
+console.log(DaysOfWeek);
+
+//  Now translate to weekday values
+// const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+ var x =[];
+ // Iterate through each ID object
+ Object.keys(DaysOfWeek).forEach(key => {
+    // Concatenate "OTU" with each ID number
+    x.push(key)
+});
+
+//TO DELETE LATER
+console.log(x);
+
+// Now translate to weekday values
+const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const x_days = x.map(day => weekdays[day - 1]);
+
+//TO DELETE LATER
+console.log(x_days);
+
+//Extracting the frequency values for our y
+ var y = [];
+    // Iterate through each ID object
+    Object.values(DaysOfWeek).forEach(value => {
+        // Concatenate "OTU" with each ID number
+        y.push(value)
+    });
+//TO DELETE LATER
+console.log(y);
+ 
+
