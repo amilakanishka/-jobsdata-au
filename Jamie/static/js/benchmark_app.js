49869bebb2dfaa10373ee86702a1a0b83e941461
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-09-22 13:45:34 +1000

    Amend Benchmark data

--- Jamie/static/js/benchmark_app.js
+++ Jamie/static/js/benchmark_app.js
@@ -5,14 +5,15 @@ d3.json(jsonPath)
     .then(data => {var jobListing = data;
 
     // Declare variables as lists for loop
-    var keywords = [];     
+    var allkeywords = [];     
     
     for (var i = 0; i < jobListing.length; i++) {
-        keywords.push(jobListing[i].keyword) // Return keyword in a list
+        allkeywords.push(jobListing[i].keyword) // Return keyword in a list
     };
 
     // Get Unique Keywords
-    var distinctKeywords = [...new Set(keywords)];
+    var distinctKeywords = [...new Set(allkeywords)];
+    console.log("distinctKeywords");
     console.log(distinctKeywords); 
 
 });
@@ -24,17 +25,64 @@ d3.csv(csvPath)
     .then(data => {var benchmarkListing = data;
 
     // Console.log to DELETE LATER
+    console.log("benchmarkListing");
     console.log(benchmarkListing);
     console.log(benchmarkListing[0]);
     console.log(benchmarkListing.length);
     
     // Need to add DOM/D3 to obtain input value from HTML
-    // TO DO - Insert codes here 
+    // Insert code here
+
+    // Temp code
+    d3.json(jsonPath)
+        .then(data => {var jobListing = data;
+
+        // var contractTypeList = jobListing.filter(item =>
+        //     (item.contract_type === "contract") &&
+        //     (item.salary_min !== ""))
+        
+        // console.log("contractTypeList");
+        // console.log(contractTypeList);
+
+        // Declare variables as lists for loop
+        var tempMSList = [];     
+        
+        for (var i = 0; i < jobListing.length; i++) {
+            if ((jobListing[i].salary_min !== "") && (jobListing[i].salary_min < 500)) {
+                tempMSList.push(jobListing[i].salary_min)} // Return keyword in a list
+        };
+
+        // Get Unique Keywords
+        var distinctMS = [...new Set(tempMSList)];
+        console.log("distinct Min Salary");
+        console.log(distinctMS); 
+        console.log(Math.max(...distinctMS));
+        console.log(Math.min(...distinctMS));
+
+        // Mock data - need to be replaced with inputValue
+        var selectedJob = jobListing[3];
+        console.log(selectedJob);
+
+        var selectedKeyword = selectedJob.Keyword;
+        // var selectedState = selectedJob.State // no such field now.. all in string
+        if (selectedJob.salary_min == "") {
+            var selectedMinSal = 0
+        } else if (selectedJob.salary_min < 500) {
+            var selectedMinSal = selectedJob.salary_min * 8 
+        } else var selectedMinSal = selectedJob.salary_min;
+        
+        console.log(selectedMinSal);
+
+        // var selectedMaxSal = selectedJob.salary_max;
+        // var selectedMedSal = (selectedMinSal + selectedMaxSal) / 2;
+        // var selectedTitle = selectedJob.title;
+    });
+    
     
     // Filter by 1) Keyword 2) State 
     var filteredbenchmarkListing = benchmarkListing.filter(item =>
         (item.Keyword === "Data Analyst") &&  // Replace with inputValue for job search keyword
-        (item.State === "New South Wales")); // Replace ith inputValue for state
+        (item.State === "Victoria")); // Replace ith inputValue for state
 
     // Filter by Contract_Type
     var permanentBenchmarkListing = filteredbenchmarkListing.filter(item =>
@@ -44,8 +92,11 @@ d3.csv(csvPath)
         (item.Contract_Type === "contract"));  
 
     // Console.log to DELETE LATER
+    console.log("filteredbenchmarkListing");
     console.log(filteredbenchmarkListing);
+    console.log("permanentBenchmarkListing");
     console.log(permanentBenchmarkListing);
+    console.log("contractBenchmarkListing");   
     console.log(contractBenchmarkListing);    
     
     var permJobRoles = permanentBenchmarkListing.map(row => row.Source + ": " + row.Job_Role);
@@ -56,6 +107,7 @@ d3.csv(csvPath)
     var permMaxSalsText = permanentBenchmarkListing.map(row => "$"+row.Max_Sal);
     var permMedSalsText = permanentBenchmarkListing.map(row => "$"+row.Median);
 
+    console.log("permtracedata");
     console.log(permMinSals);
     console.log(permJobRoles);
     console.log(permMaxSals);
@@ -66,10 +118,11 @@ d3.csv(csvPath)
         y: permJobRoles,
         text: permMinSalsText,
         name: "Minimum Salary",
+        hoverinfo: "text+name",
         orientation: "h",
         type: "bar",
         marker: {
-            color: "rgb(246,147,86)",
+            color: "rgb(35,181,181)",
             opacity: 0.0,
         }
     };
@@ -79,10 +132,16 @@ d3.csv(csvPath)
         y: permJobRoles,
         text: permMedSalsText,
         name: "Median Salary",
+        hoverinfo: "text+name",
         orientation: "h",
         type: "bar",
         marker: {
-            color: "rgb(36,110,209)",
+            color: "rgb(35,181,181)",
+            opacity: 0.6,
+            line: {
+                color: "rgb(84,84,84)",
+                width: 2,
+            }
         }
     };
 
@@ -91,10 +150,16 @@ d3.csv(csvPath)
         y: permJobRoles,
         text: permMaxSalsText,
         name: "Maximum Salary",
+        hoverinfo: "text+name",
         orientation: "h",
         type: "bar",
         marker: {
             color: "rgb(35,181,181)",
+            opacity: 0.6,
+            line: {
+                color: "rgb(84,84,84)",
+                width: 2,
+            }
         }
     };
 
@@ -109,25 +174,13 @@ d3.csv(csvPath)
         },
         xaxis: { 
             zeroline : false, 
-            showgrid : false 
+            showgrid : false ,
+            title: "Salary per annum ($AUD)",
         },
         showlegend: false
     };
 
     Plotly.newPlot("benchmark", permBenchmarkData, permBenchmarkLayout);
-
-    document.getElementById("benchmark").on('plotly_afterplot', function() {
-        var yAxisLabels = [].slice.call(document.querySelectorAll('[class^="yaxislayer"] .ytick text, [class*=" yaxislayer"] .ytick text'))
-        var bar = document.querySelector('.plot .barlayer .bars path')
-        var barHeight = bar.getBBox().height
-        var offset = 12
-        
-        for (var i = 0; i < yAxisLabels.length; i++) {
-          var yAxisLabel = yAxisLabels[i];
-          yAxisLabel.setAttribute('text-anchor', 'start')
-          yAxisLabel.setAttribute('y', yAxisLabel.getAttribute('y') - (barHeight / 2) - offset)
-        };
-    });
 
     // Setup trace and chart for contract salary
     var contractJobRoles = contractBenchmarkListing.map(row => row.Source + ": " + row.Job_Role);
@@ -138,6 +191,7 @@ d3.csv(csvPath)
     var contractMaxSalsText = contractBenchmarkListing.map(row => "$"+row.Max_Sal);
     var contractMedSalsText = contractBenchmarkListing.map(row => "$"+row.Median);
 
+    console.log("contracttracedata");
     console.log(contractMinSals);
     console.log(contractJobRoles);
     console.log(contractMaxSals);
@@ -148,10 +202,11 @@ d3.csv(csvPath)
         y: contractJobRoles,
         text: contractMinSalsText,
         name: "Minimum Salary",
+        hoverinfo: "text+name",
         orientation: "h",
         type: "bar",
         marker: {
-            color: "rgb(246,147,86)",
+            color: "rgb(35,181,181)",
             opacity: 0.0,
         }
     };
@@ -161,10 +216,16 @@ d3.csv(csvPath)
         y: contractJobRoles,
         text: contractMedSalsText,
         name: "Median Salary",
+        hoverinfo: "text+name",
         orientation: "h",
         type: "bar",
         marker: {
-            color: "rgb(36,110,209)",
+            color: "rgb(35,181,181)",
+            opacity: 0.6,
+            line: {
+                color: "rgb(84,84,84)",
+                width: 2,
+            }
         }
     };
 
@@ -173,10 +234,16 @@ d3.csv(csvPath)
         y: contractJobRoles,
         text: contractMaxSalsText,
         name: "Maximum Salary",
+        hoverinfo: "text+name",
         orientation: "h",
         type: "bar",
         marker: {
             color: "rgb(35,181,181)",
+            opacity: 0.6,
+            line: {
+                color: "rgb(84,84,84)",
+                width: 2,
+            }
         }
     };
 
@@ -191,34 +258,32 @@ d3.csv(csvPath)
         },
         xaxis: { 
             zeroline : false, 
-            showgrid : false 
+            showgrid : false,
+            title: "Contract rate per day ($AUD)", 
         },
-        showlegend: false
+        showlegend: false,
+        legend: {
+            orientation: "h",
+            xanchor: "center",
+            yanchor: "bottom",
+            x: 0.5,
+            y: -0.25,
+        }
     };
 
     Plotly.newPlot("benchmark2", contractBenchmarkData, contractBenchmarkLayout);
 
-    // document.getElementById("contract_benchmark").on('plotly_afterplot', function() {
-    //     var yAxisLabels = [].slice.call(document.querySelectorAll('[class^="yaxislayer"] .ytick text, [class*=" yaxislayer"] .ytick text'))
-    //     var bar = document.querySelector('.plot .barlayer .bars path')
-    //     var barHeight = bar.getBBox().height
-    //     var offset = 12
+    document.getElementById("benchmark").on('plotly_afterplot', function() {
+        var yAxisLabels = [].slice.call(document.querySelectorAll('[class^="yaxislayer"] .ytick text, [class*=" yaxislayer"] .ytick text'))
+        var bar = document.querySelector('.plot .barlayer .bars path')
+        var barHeight = bar.getBBox().height
+        var offset = 12
         
-    //     for (var i = 0; i < yAxisLabels.length; i++) {
-    //       var yAxisLabel = yAxisLabels[i];
-    //       yAxisLabel.setAttribute('text-anchor', 'start')
-    //       yAxisLabel.setAttribute('y', yAxisLabel.getAttribute('y') - (barHeight / 2) - offset)
-    //     };
-    // });
+        for (var i = 0; i < yAxisLabels.length; i++) {
+          var yAxisLabel = yAxisLabels[i];
+          yAxisLabel.setAttribute('text-anchor', 'start')
+          yAxisLabel.setAttribute('y', yAxisLabel.getAttribute('y') - (barHeight / 2) - offset)
+        };
+    });
 
 });
-
-
-
-// #23B5B5 light blue
-// #545454 grey
-// #F69356 orange
-// #246ED1 Darker blue
-// #89E661 Green
-// #FF5757 Coral Red
-// #017074 Turquoise
