50f8eb9fe64ffc5615f406b2b4fd6909555784fd
babetteblanquet <65524644+babetteblanquet@users.noreply.github.com>
2020-09-23 15:22:17 +1000

    Dayof the week tweak
    / in case xticks need to show

--- Babette/Job_table/table/static/js/job-app.js
+++ Babette/Job_table/table/static/js/job-app.js
@@ -16,10 +16,6 @@ d3.json("csvjson (3).json").then(function (data) {
     var tenJobs = filteredData.slice(0, 10);
     console.log(tenJobs);
 
-    // Creating a variable to get a reference to the table body
-    // var div = d3.select('#title');
-    // var div2 = d3.select('#title2');
-
     //Appending the table with each jobs data with Arrow Functions
     tenJobs.forEach((job) => {
         Object.entries(job).forEach(([key, value]) => {
@@ -45,38 +41,3 @@ d3.json("csvjson (3).json").then(function (data) {
 });
 
 
-
-
-// // Creating a variable to get a reference to the table body
-// var tbody = d3.select("tbody");
-
-// //Appending the table with each jobs data with Arrow Functions
-// tenJobs.forEach((job) => {
-//     var row = tbody.append("tr");
-//     Object.entries(job).forEach(([key, value]) => {
-//         if (key == "title") {
-//             var cell = row.append("tr");
-//             cell.text(value);
-//         }
-//     });
-
-    // Object.entries(job).forEach(([key, value]) => {
-    //     if (key == "company") {
-    //         var cell = row.append("tr");
-    //         cell.text("Company: " + value);
-    //     }
-
-    //     if (key == "description") {
-    //         var cell = row.append("tr");
-    //         cell.text(value);
-    //     }
-    //     if (key == "salary_min") {
-    //         var cell = row.append("tr");
-    //         cell.text("Salary Min: " + value);
-    //     }
-    //     if (key == "salary_max") {
-    //         var cell = row.append("tr");
-    //         cell.text("Salary Max: " + value);
-    //     }
-//     });
-// });
