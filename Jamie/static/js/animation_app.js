d9d6e66abbcbcfcd9cd5ae569c8fe47b5ccfb591
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-09-24 12:43:17 +1000

    Update animation_app.js

--- Jamie/static/js/animation_app.js
+++ Jamie/static/js/animation_app.js
@@ -1,4 +1,5 @@
 var jsonPathSalary = "historicalSalary.json"
+var csvPathUnderemployment = "Labour_csv/Underemployment_rate_Seasonally_adjusted.csv"
 
 d3.json(jsonPathSalary)
     .then(data => {var historicalSalaries = data;
@@ -6,4 +7,11 @@ d3.json(jsonPathSalary)
     console.log("historicalSalaries");
     console.log(historicalSalaries);
 
+    d3.csv(csvPathUnderemployment)
+        .then(data => {var underemploymentData = data;
+
+        console.log("underemploymentData");
+        console.log(underemploymentData);
+    
+    });
 });
